cdaec2b2a5496c7a0a079a78f79d3547a711bd5b
efeakil <efe.akil35@gmail.com>
2023-05-03 16:54:10 +0300

    custom application properties added

--- src/main/java/com/codesnitch/demo/myapp/rest/FunRestController.java
+++ src/main/java/com/codesnitch/demo/myapp/rest/FunRestController.java
@@ -1,11 +1,26 @@
 package com.codesnitch.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
     
+    //inject properties
+    @Value("${coder.nickname}")
+    private String coderNickname;
+
+    @Value("${coder.team}")
+    private String coderTeam;
+
+    //expose '/teaminfo'
+
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return "Hello " + coderNickname + ", " + coderTeam + " is your Team!"  ;
+    }
+
     // expose '/' that return Hello World
     @GetMapping("/")
     public String sayHello(){
